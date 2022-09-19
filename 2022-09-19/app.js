2f42e55428ed80cc0fb5a3c59a6fdf0e40f74c95
Sujin <ikosdu60@gmail.com>
2022-09-19 19:14:23 +0900

    [ADD] TypeScript: 타입스크립트 입문 타입스크립트 장점

---
+++ 2022-09-19/app.js
@@ -0,0 +1,29 @@
+// api url
+var url = "https://jsonplaceholder.typicode.com/users/";
+
+// dom
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var address = document.querySelector("#address");
+
+// user data
+var user = {};
+
+function startApp() {
+  axios // 함수가 실행되면 http API를 호출한다. axios는 통신기능을 제공하는 라이브러리.
+    .get(url)
+    .then(function (response) {
+      //console.log(response);
+      user = response.data;
+      // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user);
+      username.innerText = user[0].name;
+      email.innerText = user[0].email;
+      address.innerText = user[0].address.street;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+startApp();
