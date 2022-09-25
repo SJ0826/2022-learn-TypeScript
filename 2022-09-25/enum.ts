b6b5a1c17803db1ddfd43e0b8c17a52a9458a48d
Sujin <ikosdu60@gmail.com>
2022-09-25 17:02:27 +0900

    [ADD] TS: 타입스크립트 입문 이넘

---
+++ 2022-09-25/enum.ts
@@ -0,0 +1,10 @@
+// 특정한 값들의 집합
+
+enum Shoes {
+  Nike = '나이키',
+  Adidas = '아디다스',
+  
+}
+// 첫번 째는 0 두번째는 1.. 첫번째 값부터 1씩 증가하는 형태
+var myShoes = Shoes.Nike;
+console.log(myShoes); // 0 이넘을 만들때 별도의 값을 지정하지 않으면 숫자형 이넘으로 생성된다. 
